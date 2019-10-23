704326427bcfa2331bb49b0ce8b4bb54ce894565
Clênio Borges <clenioborgesx@gmail.com>
2019-10-23 10:59:04 -0300

    23/10/19

---
+++ FuncaoMultiplicar.py
@@ -0,0 +1,13 @@
+# O código cria a função multiplicação entre 2 numeros definidos a e b.
+#
+# Aluno: Clênio Borges Barboza Filho
+# Introdução a programação 2019.2
+# Professor: Francisco
+def mult(x, y):
+    return x*y
+
+a = 50
+b = 12
+r = mult(a,b) 
+
+print('Resultado = ',r)
---
+++ OrdenaLista.py
@@ -0,0 +1,35 @@
+# O código ordena uma lista de números de qualquer tipo, nesse caso float
+#
+# Aluno: Clênio Borges Barboza Filho
+# Introdução a programação 2019.2
+# Professor: Francisco
+# Criação da função ordena
+def ordenalista(lista):
+    # cópia da lista
+    r = lista
+    # laço de ordenação da lista01
+    for i in range(1, len(lista)):
+        # laço de comparação da lista
+        for j in range(i,0,-1):
+            # compara se o item é maior que outro
+            # Insertion sort
+            if(r[j]<r[j-1]):
+                # atribui uma auxiliar para lista
+                # Troca de número
+                raux = r[j]
+                r[j] = r[j-1]
+                r[j-1] = raux
+            else:
+                break
+    return r
+# Lista aleátória para teste - Random List for teste
+Lista01 = [-5,7,4,-1,0]
+
+ListaOrd = ordenalista(Lista01)
+
+print("Lista Ordenada = ", *ListaOrd)
+print("Lista Ordenada = ", ListaOrd)
+
+for i in range(0,len(ListaOrd)):
+
+    print(f"Lista Ordenada[{i}] = {ListaOrd[i]}")
