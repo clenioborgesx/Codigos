a6c7e800410a58c58f4eea1c2ee154e3331a068c
Clênio Borges <clenioborgesx@gmail.com>
2019-11-04 12:55:22 -0300

    Update Ex20-Prefeitura.py

--- Ex20-Prefeitura.py
+++ Ex20-Prefeitura.py
@@ -11,30 +11,26 @@ def mediafilhos(quantidadefilhos,i):
     somatorio=0.0
     for j in range (0,len(quantidadefilhos)):
         somatorio+=quantidadefilhos[j]
-    if j==0:
-        mediafil=somatorio/1
     else:
-        mediafil=somatorio/j
+        mediafil=somatorio/(j+1)
     return mediafil
 
 def ordenasalario(salario):
     # cópia do salario
     r = salario
-    # laço de ordenação do salario01
+    maior=r[0]
+    menor=r[0]
     for i in range(1, len(salario)):
-        # laço de comparação do salario
         for j in range(i,0,-1):
-            # compara se o item é maior que outro
-            # Insertion sort?
             if(r[j]<r[j-1]):
-                # atribui uma auxiliar para salario
-                # Troca de número
                 raux = r[j]
                 r[j] = r[j-1]
                 r[j-1] = raux
+                maior=r[j]
+                menor=r[0]
             else:
                 break
-    return r
+    return (maior,menor)
 def verificasalario(salario,quantidadefilhos):
     contador=0
     for i in range(len(salario)):
@@ -51,13 +47,11 @@ while True:
         mediapop=mediapopulacao(salario,quantidadefilhos)
         mediaf=mediafilhos(quantidadefilhos,i)
         ordenas=ordenasalario(salario)
-        maior=ordenasalario[i] #CORRIGIR
-        menor=ordenasalario[0] #CORRIGIR
         situacao=verificasalario(salario,quantidadefilhos)
-        print("A média salarial da população é: ", mediapop)
-        print("A quantidade média de filhos da população é: ", mediaf)
-        print("O maior salário e o menor salário são : ",ordenas)
-        print("A quantidade de familias recebendo até R$380,00 é: ",situacao)
+        print(f"A média salarial da população é: {mediapop:.2f}")
+        print(f"A quantidade média de filhos da população é {mediaf:.2f}")
+        print(f"O maior salário é {ordenas[0]} o menor salário é {ordenas[1]} ")
+        print(f"{situacao} familia(as) está(ão) recebendo até R$380,00")
         break
         
     else:
